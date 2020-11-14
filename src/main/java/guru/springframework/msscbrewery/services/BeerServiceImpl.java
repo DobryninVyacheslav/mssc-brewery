6de846b348eb4637f2195ce72115983f2d0cc2d7
Vyacheslav Dobrynin <dobrinin_v_j@ntik.ru>
2020-11-14 06:45:36 +0300

    added post for beer

--- src/main/java/guru/springframework/msscbrewery/services/BeerServiceImpl.java
+++ src/main/java/guru/springframework/msscbrewery/services/BeerServiceImpl.java
@@ -15,4 +15,11 @@ public class BeerServiceImpl implements BeerService {
                 .beerStyle("Pale Ale")
                 .build();
     }
+
+    @Override
+    public BeerDto saveNewDto(BeerDto beerDto) {
+        return BeerDto.builder()
+                .id(UUID.randomUUID())
+                .build();
+    }
 }
